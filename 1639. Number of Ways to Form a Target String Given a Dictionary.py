516e0512c9805486a056214c07c775a6af3c04fd
code1204 <93023818+code1204@users.noreply.github.com>
2021-10-23 09:32:14 -0400

    Update 1639. Number of Ways to Form a Target String Given a Dictionary.py
    new solution

--- 1639. Number of Ways to Form a Target String Given a Dictionary.py
+++ 1639. Number of Ways to Form a Target String Given a Dictionary.py
@@ -1,41 +1,8 @@
 def numWays(words, target):
-        MOD = 10 ** 9 + 7
-        m, n = len(words[0]), len(target)
-        #charAtIndexCnt = [[0] * m for _ in range(128)]
-        charAtIndexCnt =[]
-        #128 characters of ASCII
-        for _ in range(128):
-            charAtIndexCnt.append([0]*m)
-            
-        # Count the number of character `c` at index `i` of all words
-        for word in words:
-            for i, c in enumerate(word):
-                # ord(c):return c's ASCII number
-                charAtIndexCnt[ord(c)][i] += 1
-
-              
-
-        def dp(k, i):
-            # Formed a valid target
-            if i == n:  
-                return 1
-            # Reached to length of words[x] but don't found any result
-            if k == m:  
-                return 0
-            c = target[i]
-            # Skip k_th index of words
-            ans = dp(k + 1, i)  
-            # Take k_th index of words if found character `c` at index k_th
-            if charAtIndexCnt[ord(c)][k] > 0: 
-                ans += dp(k + 1, i + 1) * charAtIndexCnt[ord(c)][k]
-                ans %= MOD
-            return ans
-
-        return dp(0, 0)
-
-    
-def main():
-    words = ["acca","bbbb","caca"]
-    target = "aba"
-    numWays(words, target)
-main()
+        n, mod = len(target), 10**9 + 7
+        res = [1] + [0] * n
+        for i in range(len(words[0])):
+            count = collections.Counter(w[i] for w in words)
+            for j in range(n - 1, -1, -1):
+                res[j + 1] += res[j] * count[target[j]] % mod
+        return res[n] % mod
